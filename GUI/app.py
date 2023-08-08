dc949db80ad17d29132a94bf8871675f6fbf3845
chitien2808 <chitien2808@gmail.com>
2023-08-09 02:58:37 +0700

    complete Menu for Client

--- GUI/app.py
+++ GUI/app.py
@@ -2,7 +2,8 @@ from define import *
 
 class App():
     def __init__(self, ROOT) -> None:
-        ROOT.geometry('650x400')
+        ROOT.geometry('750x400')
         ROOT.title('Main Menu')
         ROOT['background'] = COLOUR_BACKGROUND
         ROOT.resizable(False, False)
+
--- GUI/define.py
+++ GUI/define.py
@@ -1,8 +1,4 @@
-from tkinter import font
-
 COLOUR_BACKGROUND = "#DDE6ED"
 COLOUR_BUTTON = "#9DB2BF"
 COLOUR_AFTER = "#526D82"
 COLOUR_FONT = "#000000"
-
-font = font.Font(family = "Terminal", size = 20)
