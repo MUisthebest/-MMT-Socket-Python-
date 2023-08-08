3010267228802b9e01f1953ce7d86f315adc0b17
chitien2808 <chitien2808@gmail.com>
2023-08-08 23:41:09 +0700

    Update define.py
    default colour, font

--- GUI/define.py
+++ GUI/define.py
@@ -1,1 +1,8 @@
-COLOUR_BACKGROUND = "#96EFFF"
+from tkinter import font
+
+COLOUR_BACKGROUND = "#DDE6ED"
+COLOUR_BUTTON = "#9DB2BF"
+COLOUR_AFTER = "#526D82"
+COLOUR_FONT = "#000000"
+
+font = font.Font(family = "Terminal", size = 20)
