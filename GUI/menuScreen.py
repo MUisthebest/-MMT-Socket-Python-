2745459e303cb21462c8332c673b120a33f52198
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2023-08-09 15:21:35 +0700

    menuScreen.py

--- GUI/menuScreen.py
+++ GUI/menuScreen.py
@@ -75,7 +75,7 @@ entryBox.grid(row = 0, column = 0, columnspan = 2, sticky = E)
 buttonConnect = Button(mainClient, text = "Connect", font = fontWord, width = 20, bg = COLOUR_BUTTON, fg = COLOUR_FONT, padx = 50, pady = 15)
 buttonConnect.grid(row = 0, column = 2)
 
-buttonProcess = Button(mainClient, text = "Process Running", font = fontWord, width = 20, bg = COLOUR_BUTTON, fg = COLOUR_FONT,command = pcs_window(), padx = 50, pady = 150)
+buttonProcess = Button(mainClient, text = "Process Running", font = fontWord, width = 20, bg = COLOUR_BUTTON, fg = COLOUR_FONT,command = lambda: pcs_window(), padx = 50, pady = 150)
 buttonProcess.grid(row = 1, column = 0, rowspan = 3)
 
 buttonApp = Button(mainClient, text = "App Running", font = fontWord, width = 26, bg = COLOUR_BUTTON, fg = COLOUR_FONT, padx = 20, pady = 33)
@@ -84,13 +84,13 @@ buttonApp.grid(row = 1, column = 1, sticky = 'EW')
 buttonTurnOff = Button(mainClient, text = "Turn-Off\n\nComputer", font = fontWord, width = 15, bg = COLOUR_BUTTON, fg = COLOUR_FONT, padx = 10, pady = 33)
 buttonTurnOff.grid(row = 2, column = 1, sticky = W)
 
-buttonCap = Button(mainClient, text = "Print\n\nScreen", font = fontWord, width = 15, bg = COLOUR_BUTTON, fg = COLOUR_FONT,command = scr_window(), padx = 10, pady = 33)
+buttonCap = Button(mainClient, text = "Print\n\nScreen", font = fontWord, width = 15, bg = COLOUR_BUTTON, fg = COLOUR_FONT,command = lambda: scr_window(), padx = 10, pady = 33)
 buttonCap.grid(row = 2, column = 1, sticky = E)
 
 buttonRegistry = Button(mainClient, text = "Fix Registry", font = fontWord, width = 26, bg = COLOUR_BUTTON, fg = COLOUR_FONT, padx = 20, pady = 33)
 buttonRegistry.grid(row = 3, column = 1, sticky = 'EW')
 
-buttonKeyStroke = Button(mainClient, text = "Keystroke", font = fontWord, width = 20, bg = COLOUR_BUTTON, fg = COLOUR_FONT,command = kst_window(), padx = 50, pady = 95)
+buttonKeyStroke = Button(mainClient, text = "Keystroke", font = fontWord, width = 20, bg = COLOUR_BUTTON, fg = COLOUR_FONT,command = lambda: kst_window(), padx = 50, pady = 95)
 buttonKeyStroke.grid(row = 1, column = 2, rowspan = 2)
 
 buttonExit = Button(mainClient, text = "Exit", font = fontWord, width = 20, bg = COLOUR_BUTTON, fg = COLOUR_FONT, padx = 50, pady = 30)
